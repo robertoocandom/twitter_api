eca27ee83f9aa6dbab3ea1f67cd894c37e085ad1
Roberto <robertoocandom@gmail.com>
2022-05-26 21:39:19 -0500

    Tweet_post

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class UserRegister(User):
 
 
 class Tweet(BaseModel):
-    twit_id: UUID = Field(...)
+    tweet_id: UUID = Field(...)
     content: str = Field(
         ...,
         min_length=1,
@@ -192,8 +192,38 @@ def home():
     summary="Post a Tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a tweet
+
+    This path operations post a tweet in the app
+
+    - Parameters:
+        - Request body parameter
+            - tweet: UserRegister
+
+    - Returns a json with the basic tweet information:
+        twit_id: UUID
+        content: str
+        created_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a Tweet
 @app.get(
